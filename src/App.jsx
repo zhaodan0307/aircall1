02d64fb29a1daa47892f941239b0067c31a665cd
Dan Zhao <dan.zhao@mygeorgian.ca>
2022-08-29 21:08:54 -0400

    sign

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Header from './Header.jsx';
+import {AllCalls} from "./components/AllCalls.jsx";
 
 const App = () => {
   return (
     <div className='container'>
       <Header/>
       <div className="container-view">Some activities should be here</div>
+        <AllCalls/>
     </div>
   );
 };
---
+++ src/components/AllCalls.jsx
@@ -0,0 +1,13 @@
+
+import * as React from 'react';
+
+export const AllCalls = () =>
+{
+
+    return (
+        <div>
+            abc
+        </div>
+
+    );
+}
